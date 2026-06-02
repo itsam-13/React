16de68035cb000edb5d2f7feea2fda4d3483b402
itsam-13 <abhineet.am13@gmail.com>
2026-06-02 17:52:49 +0530

    parameterized and non parameterized function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 const App = () => {
 
+  //non parameterized function
   const handleClick = () => {
     alert("Button clicked")
   }
+  //parameterized function
+  const handleNewClick = (msg) => {
+    alert(msg)
+  }
+  //calling a parameterized function inside a non parameterized function
+  //const wrapperHandler = () => handleNewClick("Hello, Pookie!")
 
+
+  //passed reference of the function to the button
   return (
 
     <>
+    <h1>{3+4}</h1>
     <div>APP</div>
-    <button onClick={handleClick}>Click me</button> //passed reference of the function to the button
+    <button onClick={handleClick}>Click me</button> 
+    
+    <button onClick={() => handleNewClick("Hello, Pookie!")}>Click me pookie</button>  
     
     </>
   )
