f649e7bf6e730578d9bcdcefd07746c8baa258ee
itsam-13 <abhineet.am13@gmail.com>
2026-06-02 18:41:05 +0530

    Rendering  Json

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,58 @@
+// const App = () => {
+
+//   //non parameterized function
+//   const handleClick = () => {
+//     alert("Button clicked")
+//   }
+//   //parameterized function
+//   const handleNewClick = (msg) => {
+//     alert(msg)
+//   }
+//   //calling a parameterized function inside a non parameterized function
+//   //const wrapperHandler = () => handleNewClick("Hello, Pookie!")
+
+
+//   //passed reference of the function to the button
+//   return (
+
+//     <>
+//     <h1>{3+4}</h1>
+//     <div>APP</div>
+//     <button onClick={handleClick}>Click me</button> 
+    
+//     <button onClick={() => handleNewClick("Hello, Pookie!")}>Click me pookie</button>  
+    
+//     </>
+//   )
+// }
+
+
 const App = () => {
+  const profiles = [
+    {name: "John", age: 30},
+    {name: "Jane", age: 25 },
+    {name: "Bob", age: 35}
+  ];
 
-  //non parameterized function
-  const handleClick = () => {
-    alert("Button clicked")
-  }
-  //parameterized function
-  const handleNewClick = (msg) => {
-    alert(msg)
-  }
-  //calling a parameterized function inside a non parameterized function
-  //const wrapperHandler = () => handleNewClick("Hello, Pookie!")
+  const data = profiles.map((profiles, index) =>{
+    console.log(profiles,index)
+    return (<li key = {index}>
+      <span>Name: {profiles.name}</span> | 
+      <span>Age: {profiles.age}</span>
+    </li>)
+  });
 
+console.log(data)
 
-  //passed reference of the function to the button
   return (
+    <div>
+      <h1>Rendering JSON</h1>
+      <ol>{data}</ol>
 
-    <>
-    <h1>{3+4}</h1>
-    <div>APP</div>
-    <button onClick={handleClick}>Click me</button> 
-    
-    <button onClick={() => handleNewClick("Hello, Pookie!")}>Click me pookie</button>  
-    
-    </>
+    </div>
   )
 }
 
 export default App
+
 
